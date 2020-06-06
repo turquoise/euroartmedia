d851391fa221dd3b2b98ec12089f49d930d1488b
turquoise <monicagosschalk@gmail.com>
2020-06-06 14:47:57 +0100

    changed favicon and added react helmet

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Ipad from "./Ipad";
 import Ipadpro from "./Ipadpro";
 import Laptop from "./Laptop";
 import Desktop from "./Desktop";
+import { Helmet } from "react-helmet";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Helmet
+        title="EuroArt"
+        meta={[{ name: "EuroArt Media Ltd", content: "Documentaries" }]}
+        link={[
+          { rel: "icon", type: "image/png", href: "./images/tinylogo.png" },
+        ]}
+      />
       <Mobile />
       <Ipad />
       <Ipadpro />
